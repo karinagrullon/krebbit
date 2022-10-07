b587692a118f4f61cde748a0f8e47003e645ea5b
Karina Grullon <mgrullo1@binghamton.edu>
2022-10-06 21:23:33 -0400

    adding api data file and entries

--- back-end/server.py
+++ back-end/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,17 @@ app = Flask(__name__)
 @app.route("/members")
 def members():
     return {"members": ["member 1", "member 2", "member 3"]}
+
+# Stories API route
+@app.route("/stories")
+def stories():
+    # read file
+    with open('data/data.json', 'r') as fileData:
+        data = fileData.read()
+
+    # parse file
+    obj = json.loads(data)
+    return obj
 
 if __name__ == "__main__":
     app.run(debug=True)
